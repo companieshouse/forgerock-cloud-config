34c7776e2c3b029b631faf739c4ea1cd955b65ff
Steve Clewer <steve.clewer@amido.com>
2021-02-14 22:14:41 +0000

    Fetch user's phone number from their ID

--- config/phase-0/am-scripts/scripts-content/ch-send-mfa-sms.js
+++ config/phase-0/am-scripts/scripts-content/ch-send-mfa-sms.js
@@ -5,8 +5,16 @@ var fr = JavaImporter(
 var notifyJWT = transientState.get("notifyJWT");
 var templates = transientState.get("notifyTemplates");
 var code = sharedState.get("oneTimePassword");
-var phoneNumber = "TODO";
+var userId = sharedState.get("_id");
+var phoneNumber = "";
+if (idRepository.getAttribute(userId, "telephoneNumber").iterator().hasNext()) {
+  phoneNumber = idRepository.getAttribute(userId, "telephoneNumber").iterator().next();
+} else {
+  logger.error("Couldn't find telephoneNumber");
+  // TODO Better handling of error
+}
 
+logger.error("User phoneNumber: " + phoneNumber);
 logger.error("JWT from transient state: " + notifyJWT);
 logger.error("Templates from transient state: " + templates);
 logger.error("Code: " + code);
