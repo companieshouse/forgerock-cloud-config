82aa0e1ea97db1ee145efd0ffde89d0d077967e5
Ravi Kota <55882776+Ravi-TK@users.noreply.github.com>
2021-02-22 15:06:45 +0000

    QA Tests
    Added tests to validate FR configurations

---
+++ QATests/TestRunner.js
@@ -0,0 +1,113 @@
+
+const newman = require('newman')
+
+const envs = process.argv.slice(2)
+const environment = envs[0]
+
+switch (environment) {
+  case 'dev':
+
+    newman.run({
+      collection: 'QATests\\FR Configuration test.postman_collection.json',
+      environment: 'QATests\\FR_Dev.postman_environment.json',
+      reporters: ['htmlextra'],
+      iterationCount: 1,
+      reporter: {
+        htmlextra: {
+          export: 'QATests\\Reports\\FR Congiurations Test Report- ' + environment + ' ' + Date.now() + ' .html',
+          // template: './template.hbs'
+          logs: true,
+          // showOnlyFails: true,
+          // noSyntaxHighlighting: true,
+          // testPaging: true,
+          browserTitle: 'Config Tests - Dev',
+          title: 'FR Configurations Test Report -Dev',
+          titleSize: 6,
+          // omitHeaders: true,
+          skipHeaders: ['Authorization', 'cookie'],
+          hideRequestBody: ['Access Token', 'Session Token', 'Authentication to Top-Level Realm', 'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'],
+          hideResponseBody: ['Access Token', 'Session Token', 'Authentication to Top-Level Realm', 'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'],
+          showEnvironmentData: true,
+          skipEnvironmentVars: ['IDCloudAdminUsername', 'IDCloudAdminPassword', 'username', 'password'],
+          showGlobalData: false
+          // skipGlobalVars: ["API_TOKEN"],
+          // skipSensitiveData: true,
+          // showMarkdownLinks: true,
+          // showFolderDescription: true,
+          // timezone: "Australia/Sydney"
+        }
+      }
+    })
+    break
+  case 'staging':
+    console.log(environment)
+
+    newman.run({
+      collection: 'QATests\\FR Configuration test.postman_collection.json',
+      environment: '',
+      reporters: ['htmlextra'],
+      iterationCount: 1,
+      reporter: {
+        htmlextra: {
+          export: 'QATests\\Reports\\FR Congiurations Test Report- ' + environment + ' ' + Date.now() + ' .html',
+          // template: './template.hbs'
+          logs: true,
+          // showOnlyFails: true,
+          // noSyntaxHighlighting: true,
+          // testPaging: true,
+          browserTitle: 'Config Tests - Dev',
+          title: 'FR Configurations Test Report -Dev',
+          titleSize: 6,
+          // omitHeaders: true,
+          skipHeaders: ['Authorization', 'cookie'],
+          hideRequestBody: ['Access Token', 'Session Token', 'Authentication to Top-Level Realm', 'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'],
+          hideResponseBody: ['Access Token', 'Session Token', 'Authentication to Top-Level Realm', 'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'],
+          showEnvironmentData: true,
+          skipEnvironmentVars: ['IDCloudAdminUsername', 'IDCloudAdminPassword', 'username', 'password'],
+          showGlobalData: false
+          // skipGlobalVars: ["API_TOKEN"],
+          // skipSensitiveData: true,
+          // showMarkdownLinks: true,
+          // showFolderDescription: true,
+          // timezone: "Australia/Sydney"
+        }
+      }
+    })
+    break
+  case 'production':
+
+    newman.run({
+      collection: 'QATests\\FR Configuration test.postman_collection.json',
+      environment: '',
+      reporters: ['htmlextra'],
+      iterationCount: 1,
+      reporter: {
+        htmlextra: {
+          export: 'QATests\\Reports\\FR Congiurations Test Report- ' + environment + ' ' + Date.now() + ' .html',
+          // template: './template.hbs'
+          logs: true,
+          // showOnlyFails: true,
+          // noSyntaxHighlighting: true,
+          // testPaging: true,
+          browserTitle: 'Config Tests - Dev',
+          title: 'FR Configurations Test Report -Dev',
+          titleSize: 6,
+          // omitHeaders: true,
+          skipHeaders: ['Authorization', 'cookie'],
+          hideRequestBody: ['Access Token', 'Session Token', 'Authentication to Top-Level Realm', 'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'],
+          hideResponseBody: ['Access Token', 'Session Token', 'Authentication to Top-Level Realm', 'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'],
+          showEnvironmentData: true,
+          skipEnvironmentVars: ['IDCloudAdminUsername', 'IDCloudAdminPassword', 'username', 'password'],
+          showGlobalData: false
+          // skipGlobalVars: ["API_TOKEN"],
+          // skipSensitiveData: true,
+          // showMarkdownLinks: true,
+          // showFolderDescription: true,
+          // timezone: "Australia/Sydney"
+        }
+      }
+    })
+    break
+  default:
+    console.log('Invalid Enviroment value supplied. options  => dev, stagaing, production')
+}
