7314ef72710a8a61df4406766be20f3c6f9262e8
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-03-16 16:19:45 +0000

    fixed IDM cors configuration (#43)
    * fixed IDM cors configuration
    
    * uncommented IDM rest calls to apply CORS and CORS tests

--- tests/scripts/update-cors.test.js
+++ tests/scripts/update-cors.test.js
@@ -2,11 +2,11 @@ describe('update-cors', () => {
   jest.mock('fs')
   const fs = require('fs')
   const path = require('path')
-  // jest.mock('../../helpers/get-access-token')
+  jest.mock('../../helpers/get-access-token')
   jest.mock('../../helpers/get-session-token')
   jest.mock('../../helpers/fidc-request')
   const getSessionToken = require('../../helpers/get-session-token')
-  // const getAccessToken = require('../../helpers/get-access-token')
+  const getAccessToken = require('../../helpers/get-access-token')
   const fidcRequest = require('../../helpers/fidc-request')
   const updateCors = require('../../scripts/update-cors')
   jest.spyOn(console, 'log').mockImplementation(() => {})
@@ -69,37 +69,20 @@ describe('update-cors', () => {
       }
     },
     idmCorsConfig: {
-      maxAge: 0,
-      acceptedMethods: [
-        'HEAD',
-        'DELETE',
-        'POST',
-        'GET',
-        'OPTIONS',
-        'PUT',
-        'PATCH'
-      ],
-      acceptedHeaders: [
-        'authorization',
-        'accept-api-version',
-        'content-type',
-        '*'
-      ],
-      enabled: true,
-      acceptedOrigins: [
-        'https://sdkapp.example.com:8443',
-        'http://localhost:3000',
-        'https://ui-amido-demo.forgeblocks.com',
-        'https://example.com/stoca'
-      ],
-      allowCredentials: true,
-      exposedHeaders: ['*'],
-      _id: 'org-ui',
-      _type: {
-        _id: 'configuration',
-        name: 'Cors Configuration',
-        collection: true
-      }
+      _id: 'servletfilter/cors',
+      classPathURLs: [],
+      systemProperties: {},
+      requestAttributes: {},
+      scriptExtensions: {},
+      initParams: {
+        allowedOrigins: 'https://localhost:&{openidm.port.https},https://sdkapp.example.com:8443,http://localhost:8888',
+        allowedMethods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS,HEAD',
+        allowedHeaders: 'accept,x-openidm-password,x-openidm-nosession,x-openidm-username,content-type,origin,x-requested-with,authorization',
+        allowCredentials: true,
+        chainPreflight: false
+      },
+      urlPatterns: ['/*'],
+      filterClass: 'org.eclipse.jetty.servlets.CrossOriginFilter'
     }
   }
 
@@ -108,9 +91,9 @@ describe('update-cors', () => {
     getSessionToken.mockImplementation(() =>
       Promise.resolve(mockValues.sessionToken)
     )
-    // getAccessToken.mockImplementation(() =>
-    //   Promise.resolve(mockValues.accessToken)
-    // )
+    getAccessToken.mockImplementation(() =>
+      Promise.resolve(mockValues.accessToken)
+    )
     process.env.FIDC_URL = mockValues.fidcUrl
     fs.readdirSync.mockReturnValue(['cors-config.json'])
     jest.mock(mockConfigFile, () => mockConfig, { virtual: true })
@@ -140,24 +123,24 @@ describe('update-cors', () => {
     expect(process.exit).toHaveBeenCalledWith(1)
   })
 
-  // it('should error if getAccessToken functions fails', async () => {
-  //   expect.assertions(2)
-  //   const errorMessage = 'Invalid user'
-  //   getAccessToken.mockImplementation(() =>
-  //     Promise.reject(new Error(errorMessage))
-  //   )
-  //   await updateCors(mockValues)
-  //   expect(console.error).toHaveBeenCalledWith(errorMessage)
-  //   expect(process.exit).toHaveBeenCalledWith(1)
-  // })
+  it('should error if getAccessToken functions fails', async () => {
+    expect.assertions(2)
+    const errorMessage = 'Invalid user'
+    getAccessToken.mockImplementation(() =>
+      Promise.reject(new Error(errorMessage))
+    )
+    await updateCors(mockValues)
+    expect(console.error).toHaveBeenCalledWith(errorMessage)
+    expect(process.exit).toHaveBeenCalledWith(1)
+  })
 
   it('should call AM API using config file', async () => {
-    expect.assertions(3)
+    expect.assertions(4)
     const expectedServiceUrl = `${mockValues.fidcUrl}/am/json/global-config/services/CorsService`
     const expectedServiceConfigUrl = `${expectedServiceUrl}/configuration/${mockConfig.corsServiceConfig._id}`
-    // const expectedIdmUrl = `${mockValues.fidcUrl}/openidm/config/servletfilter/cors`
+    const expectedIdmUrl = `${mockValues.fidcUrl}/openidm/config/servletfilter/cors`
     await updateCors(mockValues)
-    expect(fidcRequest.mock.calls.length).toEqual(2)
+    expect(fidcRequest.mock.calls.length).toEqual(3)
     expect(fidcRequest.mock.calls[0]).toEqual([
       expectedServiceUrl,
       mockConfig.corsServiceGlobal,
@@ -170,11 +153,12 @@ describe('update-cors', () => {
       mockValues.sessionToken,
       true
     ])
-    // expect(fidcRequest.mock.calls[2]).toEqual([
-    //   expectedIdmUrl,
-    //   mockConfig.idmCorsConfig,
-    //   mockValues.accessToken
-    // ])
+    expect(fidcRequest.mock.calls[2]).toEqual([
+      expectedIdmUrl,
+      mockConfig.idmCorsConfig,
+      mockValues.accessToken,
+      false
+    ])
   })
 
   it('should error if API request fails', async () => {
