a0b17fabb5264673171a9934d9d2a0089b9feea4
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-03-18 16:21:43 +0000

    added phone check script to config

--- config/am-scripts/scripts-content/ch-phone-number-entered.js
+++ config/am-scripts/scripts-content/ch-phone-number-entered.js
@@ -1,13 +1,11 @@
 var fr = JavaImporter(
-    org.forgerock.openam.auth.node.api.Action
+  org.forgerock.openam.auth.node.api.Action
 )
 
-var userId = sharedState.get("_id");
-if (idRepository.getAttribute(userId, "telephoneNumber").iterator().hasNext()) {
-  var phoneNumber = idRepository.getAttribute(userId, "telephoneNumber").iterator().next();
-  logger.error("Found telephoneNumber: " + phoneNumber);
+var phone = sharedState.get("objectAttributes").get("telephoneNumber");
+if (phone) {
+  transientState.put("phoneNumber", phone);
   outcome = "true";
 } else {
-  logger.error("Couldn't find telephoneNumber");
   outcome = "false";
 }
