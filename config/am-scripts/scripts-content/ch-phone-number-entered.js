3f20f3e248facc15d3cd8d34fc3f0062659dabf5
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-03-19 10:14:22 +0000

    skip the OTP flow if phone number is entered (Sprint 4)

--- config/am-scripts/scripts-content/ch-phone-number-entered.js
+++ config/am-scripts/scripts-content/ch-phone-number-entered.js
@@ -1,11 +1,9 @@
-var fr = JavaImporter(
-  org.forgerock.openam.auth.node.api.Action
-)
-
 var phone = sharedState.get("objectAttributes").get("telephoneNumber");
-if (phone) {
-  transientState.put("registrationMFA", true);
-  outcome = "true";
-} else {
-  outcome = "false";
-}
+// NB: UNCOMMENT WHEN ENABLING MFA ON REGISTRATION
+  // if (phone) {
+  //   transientState.put("registrationMFA", true);
+  //   outcome = "true";
+  // } else {
+  //   outcome = "false";
+  // }
+outcome = "false"
