b960abf0efd58d9de6bff448e492821ff108e08e
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-03-29 15:32:38 +0100

    Feature/change password (#51)
    * logic for update password
    
    * added missing callbacks classes in frImporter, fix typo
    
    * added HV callbacks for invalid password and password not matching

---
+++ config/am-scripts/scripts-content/ch-get-idm-access-token.js
@@ -0,0 +1,62 @@
+
+var clientInfoSecretString = "{\"id\": \"AMTreeAdminClient\",\"secret\": \"Passw0rd123!\",\"tokenEndpoint\": \"https://openam-companieshouse-uk-dev.id.forgerock.io/am/oauth2/realms/root/realms/alpha/access_token\",\"scope\": \"fr:idm:*\",\"serviceUsername\": \"tree-service-user@companieshouse.com\",\"servicePassword\": \"Passw0rd123!\"}"
+
+var NodeOutcome = {
+  SUCCESS: "success",
+  ERROR: "error"
+}
+
+var ACCESS_TOKEN_STATE_FIELD = "idmAccessToken"
+
+function fetchSecret() {
+  try{
+    return JSON.parse(clientInfoSecretString);
+  }catch(e){
+    logger.error("[GET IDM TOKEN] Error while parsing secret: " + e);
+    return false;
+  }
+}
+
+function logResponse(response) {
+  logger.error("[GET IDM TOKEN] HTTP Response: " + response.getStatus() + ", Body: " + response.getEntity().getString());
+}
+
+function getAccessToken() {
+    var clientInfo = fetchSecret();
+    if(!clientInfo){
+      return NodeOutcome.ERROR;
+    }
+    logger.message("[GET IDM TOKEN] Secret retrieved: "+JSON.stringify(clientInfo));
+    logger.message("[GET IDM TOKEN] Getting IDM Access Token");
+    var request = new org.forgerock.http.protocol.Request();
+    request.setUri(clientInfo.tokenEndpoint);
+    request.setMethod("POST");
+    request.getHeaders().add("Content-Type", "application/x-www-form-urlencoded");
+    var params = "grant_type=password" +
+        "&client_id=" + clientInfo.id +
+        "&client_secret=" + clientInfo.secret +
+        "&scope=" + clientInfo.scope +
+        "&username=" + clientInfo.serviceUsername +
+        "&password=" + clientInfo.servicePassword;
+    request.setEntity(params);
+    var response = httpClient.send(request).get();
+    logResponse(response);
+  
+    var oauth2response = JSON.parse(response.getEntity().getString());
+    if (!oauth2response) {
+      logger.error("Bad response")
+      return NodeOutcome.ERROR
+    }
+  
+    var accessToken = oauth2response.access_token
+    if (!accessToken) {
+      logger.error("[GET IDM TOKEN] Couldn't get access token from response")
+      return NodeOutcome.ERROR
+    }
+  
+    logger.error("[GET IDM TOKEN] Access token acquired: " + accessToken);
+    transientState.put(ACCESS_TOKEN_STATE_FIELD,accessToken);
+    return NodeOutcome.SUCCESS;
+}
+
+outcome = getAccessToken()
---
+++ config/am-scripts/scripts-content/ch-update-pwd-password-error-message.js
@@ -0,0 +1,3 @@
+sharedState.put("errorMessage","The current password you supplied is incorrect.")
+logger.error("[CHANGE PWD - DATA STORE DECISION FAIL] The current password you supplied is incorrect");
+outcome = "true";
