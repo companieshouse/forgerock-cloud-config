547b73756dff1b4ba0c1de51c7e4bd67fba1bbf6
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-03-30 15:16:50 +0100

    upgraded Create JWT for Notify script, to included non-deprecated classes, re-enabled MFA on registration

--- config/am-scripts/scripts-content/ch-phone-number-entered.js
+++ config/am-scripts/scripts-content/ch-phone-number-entered.js
@@ -1,9 +1,7 @@
 var phone = sharedState.get("objectAttributes").get("telephoneNumber");
-// NB: UNCOMMENT WHEN ENABLING MFA ON REGISTRATION
-  // if (phone) {
-  //   transientState.put("registrationMFA", true);
-  //   outcome = "true";
-  // } else {
-  //   outcome = "false";
-  // }
-outcome = "false"
+if (phone) {
+  transientState.put("registrationMFA", true);
+  outcome = "true";
+} else {
+  outcome = "false";
+}
