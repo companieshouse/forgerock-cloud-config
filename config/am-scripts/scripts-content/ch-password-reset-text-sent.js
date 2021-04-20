a617807b4d95d43f796437eb86c7fc4cbbeff83b
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-04-20 13:02:14 +0100

    fixed IDAM-365 bug, refactored 2 scripts and added docs (#78)

--- config/am-scripts/scripts-content/ch-password-reset-text-sent.js
+++ config/am-scripts/scripts-content/ch-password-reset-text-sent.js
@@ -1,26 +1,71 @@
+/* 
+  ** INPUT DATA
+    * SHARED STATE:
+      - 'oneTimePassword' : the OTP code to be sent via text
+      - '_id': the user ID to be send the text to (only populated if registrationMFA = false)
+      - 'objectAttributes.telephoneNumber': the user telephone number (entered in a previous screen)
+
+    * TRANSIENT STATE
+      - 'notificationId' : the notification ID returned by Notify if the call was successful in a previous step
+      - 'otpError': an OTP validaton error from a previous attempt (optional)  
+
+  ** OUTCOMES
+    - True: message shown 
+  
+  ** CALLBACKS:
+    - error: if no phone number was found in context
+    - error: if there is an OTP error in context (i.e. a previous attempt to validate the OTP has failed)
+    - success: if there are no errors, including the notificationID and the phoneNumber used
+*/
+
 var fr = JavaImporter(
     org.forgerock.openam.auth.node.api.Action,
     javax.security.auth.callback.TextOutputCallback,
     com.sun.identity.authentication.callbacks.HiddenValueCallback
 )
-var phoneNumber = "";
-var notificationId = transientState.get("notificationId");
-var otpError = transientState.get("error");
-logger.error("[RESET PWD] Found OTP Error : " + otpError);
 
-try{
-  var userId = sharedState.get("_id");
-  if (idRepository.getAttribute(userId, "telephoneNumber").iterator().hasNext()) {
-    phoneNumber = idRepository.getAttribute(userId, "telephoneNumber").iterator().next();
-  } else {
-    logger.error("Couldn't find telephoneNumber");
-    // TODO Better handling of error
-  }
-}catch(e){
-  logger.error("[RESET PWD] Error retrieving telephoneNumber: "+ e);
+function extractPhoneNumber(){
+    var isRegistrationMFA = transientState.get("registrationMFA");
+    var userId = sharedState.get("_id");
+    if(isRegistrationMFA){
+        return sharedState.get("objectAttributes").get("telephoneNumber");
+    } else {
+        try{
+            if (idRepository.getAttribute(userId, "telephoneNumber").iterator().hasNext()) {
+                return idRepository.getAttribute(userId, "telephoneNumber").iterator().next();
+            } else {
+                logger.error("Couldn't find telephoneNumber");
+                return false;
+            }
+        }catch(e){
+            logger.error("[SHOW PHONE NUMBER] Error retrieving telephoneNumber: "+ e);
+            return false;
+        }
+    }
 }
 
-logger.error("[RESET PWD] phoneNumber : " + phoneNumber);
+//main execution logic
+var phoneNumber = extractPhoneNumber();
+if(!phoneNumber){
+    if (callbacks.isEmpty()) {
+        action = fr.Action.send(
+            new fr.HiddenValueCallback (
+                "pagePropsJSON",
+                JSON.stringify({ 'errors': [{ label: "No phone number could be found in context.", }]})
+            ),
+            new fr.TextOutputCallback(
+                fr.TextOutputCallback.ERROR,
+                "No phone number could be found in context."
+            )
+        ).build()    
+    }
+}
+logger.error("[SHOW PHONE NUMBER] phoneNumber : " + phoneNumber);
+
+var notificationId = transientState.get("notificationId");
+var otpError = transientState.get("error");
+logger.error("[SHOW PHONE NUMBER] Notification ID: " + notificationId);
+logger.error("[SHOW PHONE NUMBER] Found OTP Error : " + otpError);
 
 if(otpError){
     if (callbacks.isEmpty()) {
--- config/am-scripts/scripts-content/ch-send-mfa-sms.js
+++ config/am-scripts/scripts-content/ch-send-mfa-sms.js
@@ -1,68 +1,37 @@
+/* 
+  ** INPUT DATA
+    * SHARED STATE:
+      - 'oneTimePassword' : the OTP code to be sent via text
+      - '_id': the user ID to be send the text to (only populated if registrationMFA = false)
+      - 'objectAttributes.telephoneNumber': the user telephone number (entered in a previous screen)
+
+    * TRANSIENT STATE
+      - 'registrationMFA' : flag indicating if this script is invoked as part of the registration journey (i.e. the user does not exist in IDM yet)
+      - 'notifyJWT': the Notify JWT to be used for requests to Notify
+      - 'templates': the list of all Notify templates 
+
+  ** OUTPUT DATA
+    * TRANSIENT STATE:
+      - 'notificationId': the notification ID returned by Notify if the call was successful
+      - 'mfa-route': the boolean indicating whether this is a SMS or a Email MFA route (SMS in this case)
+    
+
+  ** OUTCOMES
+    - true: message sent successfully
+    - false: error in sending message
+  
+  ** CALLBACKS:
+    - error (stage SEND_MFA_SMS_ERROR, error while sending SMS) 
+*/
+
 var fr = JavaImporter(
   org.forgerock.openam.auth.node.api.Action,
   javax.security.auth.callback.TextOutputCallback,
   com.sun.identity.authentication.callbacks.HiddenValueCallback
 )
 
-var notifyJWT = transientState.get("notifyJWT");
-var templates = transientState.get("notifyTemplates");
-var isRegistrationMFA = transientState.get("registrationMFA");
-var code = sharedState.get("oneTimePassword");
-var userId = sharedState.get("_id");
-var phoneNumber = "";
-
-if(isRegistrationMFA){
-  phoneNumber = sharedState.get("objectAttributes").get("telephoneNumber");
-} else {
-  if (idRepository.getAttribute(userId, "telephoneNumber").iterator().hasNext()) {
-      phoneNumber = idRepository.getAttribute(userId, "telephoneNumber").iterator().next();
-  } else {
-    logger.error("[SEND MFA SMS] Couldn't find telephoneNumber");
-    // TODO Better handling of error
-  }
-}
-
-logger.error("[SEND MFA SMS] User phoneNumber: " + phoneNumber);
-logger.error("[SEND MFA SMS] JWT from transient state: " + notifyJWT);
-logger.error("[SEND MFA SMS] Templates from transient state: " + templates);
-logger.error("[SEND MFA SMS] Code: " + code);
-
-var request = new org.forgerock.http.protocol.Request();
-request.setUri("https://api.notifications.service.gov.uk/v2/notifications/sms");
-try {
-  var requestBodyJson = {
-    "phone_number": phoneNumber,
-    "template_id": JSON.parse(templates).otpSms,
-    "personalisation": {
-        "code": code
-    }
-  }
-} catch(e) {
-  logger.error("[SEND MFA SMS] Error while preparing request for Notify: " + e);
-}
-
-request.setMethod("POST");
-request.getHeaders().add("Content-Type", "application/json");
-request.getHeaders().add("Authorization", "Bearer " + notifyJWT);
-request.getEntity().setString(JSON.stringify(requestBodyJson))
-
-var notificationId;
-var response = httpClient.send(request).get();
-
-try {
-  notificationId = JSON.parse(response.getEntity().getString()).id;
-  logger.error("[SEND MFA SMS] Notify ID: " + notificationId);
-  transientState.put("notificationId", notificationId);
-  transientState.put("mfa-route", "sms");
-} catch(e) {
-  logger.error("[SEND MFA SMS] Error while parsing Notify response: " + e);
-}
-
-logger.error("[SEND MFA SMS] Notify Response: " + response.getStatus().getCode() + response.getCause() + response.getEntity().getString());
-
-if (response.getStatus().getCode() == 201) {
-  outcome = "true";
-} else {
+// sends the error callbacks
+function sendErrorCallbacks(){
   if (callbacks.isEmpty()) {
     action = fr.Action.send(
       new fr.HiddenValueCallback (
@@ -79,5 +48,86 @@ if (response.getStatus().getCode() == 201) {
       )
     ).build()
   }
-  outcome = "false";
+}
+
+// sends the OTP code via text to the number specified
+function sendTextMessage(phoneNumber, code){
+  var notifyJWT = transientState.get("notifyJWT");
+  var templates = transientState.get("notifyTemplates");
+  logger.error("[SEND MFA SMS] JWT from transient state: " + notifyJWT);
+  logger.error("[SEND MFA SMS] Templates from transient state: " + templates);
+  var request = new org.forgerock.http.protocol.Request();
+  request.setUri("https://api.notifications.service.gov.uk/v2/notifications/sms");
+  try {
+    var requestBodyJson = {
+      "phone_number": phoneNumber,
+      "template_id": JSON.parse(templates).otpSms,
+      "personalisation": {
+          "code": code
+      }
+    }
+  } catch(e) {
+    logger.error("[SEND MFA SMS] Error while preparing request for Notify: " + e);
+    return false;
+  }
+
+  request.setMethod("POST");
+  request.getHeaders().add("Content-Type", "application/json");
+  request.getHeaders().add("Authorization", "Bearer " + notifyJWT);
+  request.getEntity().setString(JSON.stringify(requestBodyJson))
+
+  var notificationId;
+  var response = httpClient.send(request).get();
+  logger.error("[SEND MFA SMS] Notify Response: " + response.getStatus().getCode() + response.getCause() + response.getEntity().getString());
+
+  try {
+    notificationId = JSON.parse(response.getEntity().getString()).id;
+    logger.error("[SEND MFA SMS] Notify ID: " + notificationId);
+    transientState.put("notificationId", notificationId);
+    transientState.put("mfa-route", "sms");
+  } catch(e) {
+    logger.error("[SEND MFA SMS] Error while parsing Notify response: " + e);
+    return false;
+  }
+
+  logger.error("[SEND MFA SMS] Notify Response: " + response.getStatus().getCode() + response.getCause() + response.getEntity().getString());
+
+  if (response.getStatus().getCode() == 201) {    
+    return true;;
+  } 
+  
+  return false;
+}
+
+// extracts the number from the user profile (for password reset) or from shared state (for registration)
+function extractPhoneNumber(){
+  var userId = sharedState.get("_id");
+  var isRegistrationMFA = transientState.get("registrationMFA");
+  if(isRegistrationMFA){
+    return sharedState.get("objectAttributes").get("telephoneNumber");
+  } else {
+    if (idRepository.getAttribute(userId, "telephoneNumber").iterator().hasNext()) {
+        return idRepository.getAttribute(userId, "telephoneNumber").iterator().next();
+    } else {
+      logger.error("[SEND MFA SMS] Couldn't find telephoneNumber");
+      return false;
+    }
+  }
+}
+
+// main execution flow
+var code = sharedState.get("oneTimePassword");
+logger.error("[SEND MFA SMS] Code: " + code);
+
+var phoneNumber = extractPhoneNumber();
+if(!phoneNumber || !code){
+  sendErrorCallbacks();
+}
+
+logger.error("[SEND MFA SMS] User phoneNumber: " + phoneNumber);
+
+if (sendTextMessage(phoneNumber, code)) {
+  action = fr.Action.goTo("true").build();
+} else {
+  sendErrorCallbacks();
 }
