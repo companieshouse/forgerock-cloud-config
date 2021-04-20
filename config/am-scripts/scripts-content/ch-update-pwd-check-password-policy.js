951640b4851e508772c39dec19ade448b0c0c0c9
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-04-20 09:24:54 +0100

    added more error handling logic

--- config/am-scripts/scripts-content/ch-update-pwd-check-password-policy.js
+++ config/am-scripts/scripts-content/ch-update-pwd-check-password-policy.js
@@ -1,25 +1,27 @@
 var ACCESS_TOKEN_STATE_FIELD = "idmAccessToken"
 var idmEndpoint = "https://openam-companieshouse-uk-dev.id.forgerock.io/openidm/policy/managed/alpha_user/*?_action=validateProperty"
-
+​
 var NodeOutcome = {
   PASS: "pass",
   FAIL: "fail",
   ERROR: "error"
 }
-
-var VAR_PASSWORD = "newPassword"
-var VAR_POLICY_ERROR_FIELD = "errorMessage"
-
+​
+var VAR_PASSWORD = "newPassword";
+var VAR_POLICY_ERROR_FIELD = "errorMessage";
+var VAR_POLICY_ERROR_FIELD_ID = "changePwdErrorField";
+​
 function logResponse(response) {
     logger.error("[CHANGE PWD - POLICY CHECK] Scripted Node HTTP Response: " + response.getStatus() + ", Body: " + response.getEntity().getString());
 }
-
+​
 function setPolicyErrorMessage(policyResponse) {
   // Add code here if more granular advice required
   var errorMessage = "The new password does not meet the password policy requirements."
-  sharedState.put(VAR_POLICY_ERROR_FIELD,errorMessage)
+  sharedState.put(VAR_POLICY_ERROR_FIELD, errorMessage);
+  sharedState.put(VAR_POLICY_ERROR_FIELD_ID, "IDToken3");
 }
-
+​
 function policyCompliant(pwd) {
     logger.error("[CHANGE PWD - POLICY CHECK] Checking password [" + pwd + "] against policy");
     var accessToken = transientState.get(ACCESS_TOKEN_STATE_FIELD);
@@ -28,7 +30,7 @@ function policyCompliant(pwd) {
       return NodeOutcome.ERROR
     }
     
-
+​
     var request = new org.forgerock.http.protocol.Request();
     var requestBodyJson = {
       "object": {},
@@ -43,7 +45,7 @@ function policyCompliant(pwd) {
     request.setEntity(requestBodyJson);
     
     var response = httpClient.send(request).get();
-
+​
     logResponse(response);
     if (response.getStatus().getCode() === 200) {
         logger.error("[CHANGE PWD - POLICY CHECK] 200 response from IDM");
@@ -71,11 +73,11 @@ function policyCompliant(pwd) {
     logger.error("[CHANGE PWD - POLICY CHECK] Error");
     return NodeOutcome.ERROR; 
 }
-
+​
 // Main execution path
                                    
 var newPassword = transientState.get(VAR_PASSWORD)
-
+​
 if (newPassword == null) {
   logger.error("[CHANGE PWD - POLICY CHECK] No password in shared state for policy evaluation");
   outcome = NodeOutcome.ERROR
