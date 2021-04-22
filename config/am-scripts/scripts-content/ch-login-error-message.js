2c2887779642efbd97f94c3b899a598a2d71b488
Steve Clewer <steve.clewer@amido.com>
2021-04-22 14:49:15 +0100

    Minor tidy up

--- config/am-scripts/scripts-content/ch-login-error-message.js
+++ config/am-scripts/scripts-content/ch-login-error-message.js
@@ -1,11 +1,6 @@
 var username = sharedState.get("username");
 var password = transientState.get("password");
 
-logger.error("[LOGIN] " + "Shared state: " + sharedState.toString() + "\\n");
-logger.error("[LOGIN] " + "Transient state: " + transientState.toString() + "\\n");
-
-logger.error("[LOGIN] " + username + " " + password);
-
 if (username && password) {
     sharedState.put("errorMessage","Enter a correct username and password.")
     sharedState.put("loginErrorType", "USER_CREDENTIALS_INCORRECT");
