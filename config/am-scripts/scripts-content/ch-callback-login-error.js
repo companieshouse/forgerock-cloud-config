d0163c7b1d93f363ea4923665e97b023b2e8dad8
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-05-04 12:29:42 +0100

    removed unnecessary try/catch

--- config/am-scripts/scripts-content/ch-callback-login-error.js
+++ config/am-scripts/scripts-content/ch-callback-login-error.js
@@ -18,7 +18,6 @@ var fr = JavaImporter(
   
 logger.error("[LOGIN ERROR CALLBACK] Enter a correct username and password.");
 
-try{
 if (callbacks.isEmpty()) {
     var errorMessage = sharedState.get("errorMessage");
     var level = fr.TextOutputCallback.INFORMATION;
@@ -33,9 +32,6 @@ if (callbacks.isEmpty()) {
         new fr.HiddenValueCallback("pagePropsJSON", errorProps)
         ).build();
     }
-}
-}catch(e){
-  logger.error("[LOGIN ERROR CALLBACK] error - "+e);
 }
 
 outcome = "true";
