2632189f43ccb120ddab6edb64bd5b9d69982401
Saul France <saul.france@amido.com>
2021-05-11 17:12:08 +0100

    use template file and replacement helper function

--- qa-tests/TestRunner.js
+++ qa-tests/TestRunner.js
@@ -1,66 +1,109 @@
 const newman = require('newman')
+const replaceSensitiveValues = require('../helpers/replace-sensitive-values')
 
-newman.run(
-  {
-    collection: 'qa-tests/TestCollection.postman_collection.json',
-    environment: 'qa-tests/env.postman_environment.json',
-    reporters: ['htmlextra'],
-    iterationCount: 1,
-    reporter: {
-      htmlextra: {
-        export: 'qa-tests/Reports/fidc-config-tests-report.html',
-        // template: './template.hbs'
-        logs: true,
-        // showOnlyFails: true,
-        // noSyntaxHighlighting: true,
-        // testPaging: true,
-        browserTitle: 'FR Config Tests',
-        title: 'FR Configurations Test Report',
-        titleSize: 6,
-        // omitHeaders: true,
-        skipHeaders: ['Authorization', 'cookie'],
-        hideRequestBody: [
-          'Access Token',
-          'Session Token',
-          'Skip MFA Set up',
-          'Authentication to Top-Level Realm',
-          'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'
-        ],
-        hideResponseBody: [
-          'Access Token',
-          'Session Token',
-          'Skip MFA Set up',
-          'CHSUser Connector',
-          'CHSRoles Connector',
-          'Authentication to Top-Level Realm',
-          'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'
-        ],
-        showEnvironmentData: true,
-        skipEnvironmentVars: [
-          'access_token',
-          'ClientId',
-          'ClientSecret',
-          'session_token',
-          'cookie_Name',
-          'cookie_Name',
-          'IDCloudAdminUsername',
-          'IDCloudAdminPassword',
-          'username',
-          'password',
-          'utils'
-        ],
-        showGlobalData: false
-        // skipGlobalVars: ["API_TOKEN"],
-        // skipSensitiveData: true,
-        // showMarkdownLinks: true,
-        // showFolderDescription: true,
-        // timezone: "Australia/Sydney"
+const testRunner = async () => {
+  const {
+    PLATFORM_URL,
+    CLIENT_ID,
+    IDM_USERNAME,
+    IDM_PASSWORD,
+    REALM,
+    ID_CLOUD_ADMIN_USERNAME,
+    ID_CLOUD_ADMIN_PASSWORD,
+    COOKIE,
+    VERSION
+  } = process.env
+
+  await replaceSensitiveValues(
+    __dirname,
+    [
+      '{REPLACEMENT_PLATFORM_URL}',
+      '{REPLACEMENT_CLIENT_ID}',
+      '{REPLACEMENT_IDM_USERNAME}',
+      '{REPLACEMENT_IDM_PASSWORD}',
+      '{REPLACEMENT_REALM}',
+      '{REPLACEMENT_ID_CLOUD_ADMIN_USERNAME}',
+      '{REPLACEMENT_ID_CLOUD_ADMIN_PASSWORD}',
+      '{REPLACEMENT_COOKIE}',
+      '{REPLACEMENT_VERSION}'
+    ],
+    [
+      PLATFORM_URL,
+      CLIENT_ID,
+      IDM_USERNAME,
+      IDM_PASSWORD,
+      REALM,
+      ID_CLOUD_ADMIN_USERNAME,
+      ID_CLOUD_ADMIN_PASSWORD,
+      COOKIE,
+      VERSION
+    ]
+  )
+
+  newman.run(
+    {
+      collection: 'qa-tests/TestCollection.postman_collection.json',
+      environment: 'qa-tests/env.postman_environment.json',
+      reporters: ['htmlextra'],
+      iterationCount: 1,
+      reporter: {
+        htmlextra: {
+          export: 'qa-tests/Reports/fidc-config-tests-report.html',
+          // template: './template.hbs'
+          logs: true,
+          // showOnlyFails: true,
+          // noSyntaxHighlighting: true,
+          // testPaging: true,
+          browserTitle: 'FR Config Tests',
+          title: 'FR Configurations Test Report',
+          titleSize: 6,
+          // omitHeaders: true,
+          skipHeaders: ['Authorization', 'cookie'],
+          hideRequestBody: [
+            'Access Token',
+            'Session Token',
+            'Skip MFA Set up',
+            'Authentication to Top-Level Realm',
+            'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'
+          ],
+          hideResponseBody: [
+            'Access Token',
+            'Session Token',
+            'Skip MFA Set up',
+            'CHSUser Connector',
+            'CHSRoles Connector',
+            'Authentication to Top-Level Realm',
+            'Authenticate as ID Cloud Admin and Get Session Token, and Cookie Name'
+          ],
+          showEnvironmentData: true,
+          skipEnvironmentVars: [
+            'access_token',
+            'ClientId',
+            'ClientSecret',
+            'session_token',
+            'cookie_Name',
+            'cookie_Name',
+            'IDCloudAdminUsername',
+            'IDCloudAdminPassword',
+            'username',
+            'password',
+            'utils'
+          ],
+          showGlobalData: false
+          // skipGlobalVars: ["API_TOKEN"],
+          // skipSensitiveData: true,
+          // showMarkdownLinks: true,
+          // showFolderDescription: true,
+          // timezone: "Australia/Sydney"
+        }
+      }
+    },
+    function (err, summary) {
+      if (err || summary.run.error || summary.run.failures.length) {
+        process.exit(1)
       }
     }
-  },
-  function (err, summary) {
-    if (err || summary.run.error || summary.run.failures.length) {
-      process.exit(1)
-    }
-  }
-)
+  )
+}
+
+testRunner()
---
+++ qa-tests/env.postman_environment.json.tpl
@@ -0,0 +1,69 @@
+{
+  "id": "ee33f01e-de2b-4586-9d30-6873b05b1364",
+  "name": "FREnvironment",
+  "values": [
+    {
+      "key": "platformUrl",
+      "value": "{REPLACEMENT_PLATFORM_URL}",
+      "enabled": true
+    },
+    {
+      "key": "ClientId",
+      "value": "{REPLACEMENT_CLIENT_ID}",
+      "enabled": true
+    },
+    {
+      "key": "ClientSecret",
+      "value": "null",
+      "enabled": true
+    },
+    {
+      "key": "username",
+      "value": "{REPLACEMENT_IDM_USERNAME}",
+      "enabled": true
+    },
+    {
+      "key": "password",
+      "value": "{REPLACEMENT_IDM_PASSWORD}",
+      "enabled": true
+    },
+    {
+      "key": "realm",
+      "value": "{REPLACEMENT_REALM}",
+      "enabled": true
+    },
+    {
+      "key": "access_token",
+      "value": "",
+      "enabled": true
+    },
+    {
+      "key": "session_token",
+      "value": "",
+      "enabled": true
+    },
+    {
+      "key": "IDCloudAdminUsername",
+      "value": "{REPLACEMENT_ID_CLOUD_ADMIN_USERNAME}",
+      "enabled": true
+    },
+    {
+      "key": "IDCloudAdminPassword",
+      "value": "{REPLACEMENT_ID_CLOUD_ADMIN_PASSWORD}",
+      "enabled": true
+    },
+    {
+      "key": "cookie_Name",
+      "value": "{REPLACEMENT_COOKIE}",
+      "enabled": true
+    },
+    {
+      "key": "version",
+      "value": "{REPLACEMENT_VERSION}",
+      "enabled": true
+    }
+  ],
+  "_postman_variable_scope": "environment",
+  "_postman_exported_at": "2021-04-07T13:02:19.538Z",
+  "_postman_exported_using": "Postman/8.1.0"
+}
