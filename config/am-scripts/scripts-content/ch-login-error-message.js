643b09ff0b4b2aa3de9d5a35d83672cd27aa24e5
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2021-06-08 13:12:02 +0100

    added extra script to manage credentials format check, changed login journeys accordingly

--- config/am-scripts/scripts-content/ch-login-error-message.js
+++ config/am-scripts/scripts-content/ch-login-error-message.js
@@ -1,30 +1,13 @@
-var username = sharedState.get("username");
-var password = transientState.get("password");
-
-logger.error("[LOGIN] Enter correct credentials.");
-
-if (username && password) {
-    sharedState.put("errorMessage", "Enter a correct username and password.")
-    sharedState.put("pagePropsJSON", JSON.stringify(
-        {
-            'errors': [{
-                label: "Enter a correct username and password.",
-                token: "USER_CREDENTIALS_INCORRECT",
-                fieldName: "IDToken2",
-                anchor: "IDToken2"
-            }]
-        }));
-} else {
-    sharedState.put("errorMessage", "Enter a username and password.")
-    sharedState.put("pagePropsJSON", JSON.stringify(
-        {
-            'errors': [{
-                label: "Enter a username and password.",
-                token: "USER_CREDENTIALS_INCOMPLETE",
-                fieldName: "IDToken1",
-                anchor: "IDToken1"
-            }]
-        }));
-}
+logger.error("[LOGIN] Cannot find a user with this email.");
+sharedState.put("errorMessage", "Cannot find a user with this email.")
+sharedState.put("pagePropsJSON", JSON.stringify(
+    {
+        'errors': [{
+            label: "Cannot find a user with this email.",
+            token: "USER_EMAIL_NOT_FOUND",
+            fieldName: "IDToken2",
+            anchor: "IDToken2"
+        }]
+    }));
 
 outcome = "true";
---
+++ config/am-scripts/scripts-content/ch-login-input-check.js
@@ -0,0 +1,58 @@
+var username = sharedState.get("username");
+var password = transientState.get("password");
+
+// validates email format
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+var NodeOutcome = {
+    TRUE: "true",
+    FALSE: "false"
+}
+
+try{
+    var errorMessage = "";
+    //both credentials are supplied
+    if (username && password) { 
+        // email has wrong format
+        if (!validateEmail(username)) {
+            errorMessage = errorMessage.concat("Invalid email format: ").concat(username);
+            logger.error("[LOGIN] invalid email format");
+            sharedState.put("errorMessage", errorMessage);
+            sharedState.put("pagePropsJSON", JSON.stringify(
+                {
+                    'errors': [{
+                        label: errorMessage,
+                        token: "EMAIL_FORMAT_ERROR",
+                        fieldName: "IDToken1",
+                        anchor: "IDToken1"
+                    }]
+                }));
+            outcome = NodeOutcome.FALSE;
+        } else {
+            outcome = NodeOutcome.TRUE;
+        }        
+    } else if (!username || !password) {
+
+        logger.error("[LOGIN] username or pwd missing");
+
+        sharedState.put("errorMessage", "Username or password missing.")
+        sharedState.put("pagePropsJSON", JSON.stringify(
+            {
+                'errors': [{
+                    label: "Username or password missing.",
+                    token: "USER_CREDENTIALS_INCOMPLETE",
+                    fieldName: "IDToken1",
+                    anchor: "IDToken1"
+                }]
+            }));
+        outcome = NodeOutcome.FALSE;
+    } else {
+        outcome = NodeOutcome.TRUE;
+    }
+
+}catch(e){
+    logger.error("error: "+e)
+}
