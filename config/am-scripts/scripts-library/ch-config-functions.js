49a7a429f8d8bbcb9e1afb0084d9c4bcbf87815c
Steve Davis <sdavis@companieshouse.gov.uk>
2021-12-09 13:47:37 +0000

    idam-1034/seperate config file

--- config/am-scripts/scripts-library/ch-config-functions.js
+++ config/am-scripts/scripts-library/ch-config-functions.js
@@ -1,20 +1,15 @@
-var _CONFIG_FIDC_ENDPOINT = 'https://openam-companieshouse-uk-dev.id.forgerock.io';
-var _CONFIG_NOTIFY_EMAIL_ENDPOINT = 'https://api.notifications.service.gov.uk/v2/notifications/email';
-var _CONFIG_NOTIFY_SMS_ENDPOINT = 'https://api.notifications.service.gov.uk/v2/notifications/sms';
-var _CONFIG_VALIDATE_SERVICE_SECRET_ENDPOINT = 'https://btazausqwf.execute-api.eu-west-2.amazonaws.com/cidev/';
-
-function _fromConfig (configElement, defaultValue) {
-  //const _CONFIG_FIDC_ENDPOINT = 'https://openam-companieshouse-uk-dev.id.forgerock.io';
-  //const _CONFIG_NOTIFY_EMAIL_ENDPOINT = 'https://api.notifications.service.gov.uk/v2/notifications/email';
-  //const _CONFIG_NOTIFY_SMS_ENDPOINT = 'https://api.notifications.service.gov.uk/v2/notifications/sms';
-  //const _CONFIG_VALIDATE_SERVICE_SECRET_ENDPOINT = 'https://btazausqwf.execute-api.eu-west-2.amazonaws.com/cidev/';
+function _fromConfig (configElement) {
+  var _CONFIG_FIDC_ENDPOINT = 'https://openam-companieshouse-uk-dev.id.forgerock.io';
+  var _CONFIG_NOTIFY_EMAIL_ENDPOINT = 'https://api.notifications.service.gov.uk/v2/notifications/email';
+  var _CONFIG_NOTIFY_SMS_ENDPOINT = 'https://api.notifications.service.gov.uk/v2/notifications/sms';
+  var _CONFIG_VALIDATE_SERVICE_SECRET_ENDPOINT = 'https://btazausqwf.execute-api.eu-west-2.amazonaws.com/cidev/';
 
   var allConfigItems = {
-    _CONFIG_FIDC_ENDPOINT: _CONFIG_FIDC_ENDPOINT,
-    _CONFIG_NOTIFY_EMAIL_ENDPOINT: _CONFIG_NOTIFY_EMAIL_ENDPOINT,
-    _CONFIG_NOTIFY_SMS_ENDPOINT: _CONFIG_NOTIFY_SMS_ENDPOINT,
-    _CONFIG_VALIDATE_SERVICE_SECRET_ENDPOINT: _CONFIG_VALIDATE_SERVICE_SECRET_ENDPOINT
+    _ITEM_FIDC_ENDPOINT: _CONFIG_FIDC_ENDPOINT,
+    _ITEM_NOTIFY_EMAIL_ENDPOINT: _CONFIG_NOTIFY_EMAIL_ENDPOINT,
+    _ITEM_NOTIFY_SMS_ENDPOINT: _CONFIG_NOTIFY_SMS_ENDPOINT,
+    _ITEM_VALIDATE_SERVICE_SECRET_ENDPOINT: _CONFIG_VALIDATE_SERVICE_SECRET_ENDPOINT
   };
 
-  return allConfigItems['_CONFIG_' + configElement] || '';
+  return allConfigItems['_ITEM_' + configElement] || ('Unknown config item : ' + configElement);
 }
