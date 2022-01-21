2c3e56904eda01bc6541cacc3fcd11a2358f5151
Steve Davis <sdavis@companieshouse.gov.uk>
2022-01-21 11:08:48 +0000

    idam-905/Pass back tokens for various password UI issues

--- config/am-scripts/scripts-content/ch-update-pwd-input-collector.js
+++ config/am-scripts/scripts-content/ch-update-pwd-input-collector.js
@@ -18,10 +18,12 @@ if (callbacks.isEmpty()) {
   var infoMessage = 'Please enter your current password, together with your new password (and confirmation)';
   var level = fr.TextOutputCallback.INFORMATION;
   var errorMessage = sharedState.get('errorMessage');
+
   if (errorMessage !== null) {
     var errorProps = sharedState.get('pagePropsJSON');
     level = fr.TextOutputCallback.ERROR;
     infoMessage = errorMessage.concat(' Please try again.');
+
     action = fr.Action.send(
       fr.TextOutputCallback(level, infoMessage),
       fr.PasswordCallback('Current password', false),
@@ -30,7 +32,9 @@ if (callbacks.isEmpty()) {
       fr.HiddenValueCallback('stage', 'CHANGE_PASSWORD_1'),
       fr.HiddenValueCallback('pagePropsJSON', errorProps)
     ).build();
+
   } else {
+
     action = fr.Action.send(
       fr.TextOutputCallback(level, infoMessage),
       fr.PasswordCallback('Current password', false),
@@ -38,27 +42,69 @@ if (callbacks.isEmpty()) {
       fr.PasswordCallback('Confirm new password', false),
       fr.HiddenValueCallback('stage', 'CHANGE_PASSWORD_1')
     ).build();
+
   }
 } else {
+
   var password = fr.String(callbacks.get(1).getPassword());
   var newPassword = fr.String(callbacks.get(2).getPassword());
   var confirmNewPassword = fr.String(callbacks.get(3).getPassword());
-  if (!confirmNewPassword.equals(newPassword)) {
-    sharedState.put('errorMessage', 'The new password and confirmation do not match.');
+
+  if (newPassword && newPassword.trim().length === 0) {
+    newPassword = null;
+  }
+
+  if (confirmNewPassword && confirmNewPassword.trim().length === 0) {
+    confirmNewPassword = null;
+  }
+
+  var errors = [];
+
+  if (!newPassword) {
+    errors.push({
+      label: 'The new password cannot be empty.',
+      token: 'CREATE_PASSWORD_REQUIRED',
+      fieldName: 'IDToken2',
+      anchor: 'IDToken2'
+    });
+  }
+
+  if (!confirmNewPassword) {
+    errors.push({
+      label: 'The confirmation password cannot be empty.',
+      token: 'CREATE_REENTER_PASSWORD_REQUIRED',
+      fieldName: 'IDToken3',
+      anchor: 'IDToken3'
+    });
+  }
+
+  if (newPassword && confirmNewPassword && (newPassword !== confirmNewPassword)) {
+    errors.push({
+      label: 'The new password and confirmation do not match.',
+      token: 'PWD_MISMATCH',
+      fieldName: 'IDToken3',
+      anchor: 'IDToken3'
+    });
+  }
+
+  if (errors.length > 0) {
+
+    sharedState.put('errorMessage', 'There is an issue with the password supplied for update.');
+
     sharedState.put('pagePropsJSON', JSON.stringify(
       {
-        'errors': [{
-          label: 'The new password and confirmation do not match.',
-          token: 'PWD_MISMATCH',
-          fieldName: 'IDToken3',
-          anchor: 'IDToken3'
-        }]
+        'errors': errors
       }));
+
     action = fr.Action.goTo(NodeOutcome.MISMATCH).build();
+
   } else {
+
     transientState.put('password', password);
     transientState.put('newPassword', newPassword);
+
     action = fr.Action.goTo(NodeOutcome.SUCCESS).build();
+
   }
 }
 
