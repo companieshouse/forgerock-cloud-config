957ec203eaac7ddd301110933158886ccecefd55
matteoformica-amido <46925625+matteoformica-amido@users.noreply.github.com>
2022-01-27 11:35:14 +0000

    changes to enable usage of API for hash gen (#387)
    - added hash gen API url and key to ESVs
    - changed onCreate/onUpdate scripts to use the ESVs
    - improved logging

--- config/managed-objects/event-scripts/alpha_user_onCreate.js
+++ config/managed-objects/event-scripts/alpha_user_onCreate.js
@@ -4,61 +4,65 @@ logger.info('Setting email as username ' + object.mail);
 object.mail = object.userName;
 object.sn = object.userName;
 
+var HASH_CONVERT_API_URL = identityServer.getProperty('esv.5759beed9b.generatehashapiurl');
+var HASH_CONVERT_API_KEY = identityServer.getProperty('esv.cbd3eb4482.generatehashapikey');
+
 var request = {
-    'url': 'https://ypdak57qu6.execute-api.eu-west-1.amazonaws.com/default/dummyBCryptValue',
+    'url': HASH_CONVERT_API_URL,
     'method': 'POST',
     'headers': {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'x-api-key' : HASH_CONVERT_API_KEY
     },
     'body': '{"convert": "' + object.password + '"}'
 };
 
-logger.error('OnCreate PARENT_USERNAME: {}', object.frIndexedString1);
-logger.error('OnCreate LEGACY_PASSWORD: {}', object.frIndexedString2);
+logger.debug('OnCreate current PARENT_USERNAME: {}', object.frIndexedString1);
+logger.debug('OnCreate current LEGACY_PASSWORD: {}', object.frIndexedString2);
 
-if (object.frIndexedString1 != null) {
-    logger.error('OnCreate already has PARENT_USERNAME: {}', object.frIndexedString1);
-} else {
-    logger.error('OnCreate generate MD5');
-    //hashTest = openidm.hash(object.mail, "MD5");
-    hashTest = getRandomHex(32);
-    logger.error('OnCreate generated PARENT_USERNAME hash ' + hashTest);
+try {
+    if (object.frIndexedString1 != null) {
+        logger.info('OnCreate the user already has PARENT_USERNAME: {}', object.frIndexedString1);
+    } else {
+        hashTest = getRandomHex(32);
+        logger.debug('OnCreate generated PARENT_USERNAME: ' + hashTest);
 
-    // Update the field with the hash
-    //object.frIndexedString1 = hashTest.$crypto.value.data;
-    object.frIndexedString1 = hashTest;
-}
+        // Update the field with the hash
+        object.frIndexedString1 = hashTest;
+    }
 
-if (object.frIndexedString2 != null) {
-    // User is created with legacy password from WebFiling - leave it as is
-    logger.error('OnCreate already has LEGACY_PASSWORD: {}', object.frIndexedString2);
-
-    // Update related params so we know what type of BCrypt hash it is
-    object.frIndexedString3 = 'pending';
-    object.frIndexedString5 = 'webfiling';
-} else {
-    try {
-        logger.debug('OnCreate LEGACY_PASSWORD request: {}', request);
-        var result = openidm.action('external/rest', 'call', request);
-        logger.debug('OnCreate LEGACY_PASSWORD result: {}', result);
-
-        var resultJson = JSON.parse(result);
-        var bodyJson = JSON.parse(resultJson.body);
-
-        object.frIndexedString2 = bodyJson.hash;
+    if (object.frIndexedString2 != null) {
+        // User is created with legacy password from WebFiling - leave it as is
+        logger.info('OnCreate the user already has LEGACY_PASSWORD: {}', object.frIndexedString2);
 
         // Update related params so we know what type of BCrypt hash it is
-        object.frIndexedString3 = 'migrated';
+        object.frIndexedString3 = 'pending';
         object.frIndexedString5 = 'webfiling';
-    } catch (e) {
-        logger.info('OnCreate LEGACY_PASSWORD ERROR');
-        if (e.javaException.getCode() === '201') {
-            logger.info('OnCreate LEGACY_PASSWORD CODE HTTP: 201');
-        } else if (e.javaException.getCode() === '400') {
-            logger.info('OnCreate CODE HTTP: 400');
+    } else {
+        logger.info('OnCreate the user does not have a LEGACY_PASSWORD set: generating one');
+        if(!HASH_CONVERT_API_URL || !HASH_CONVERT_API_KEY){
+            logger.error('OnCreate could not find Hash Generation API details - LEGACY PASSWORD not generated');
+        } else {
+            try {
+                logger.debug('OnCreate LEGACY_PASSWORD request: {}', request);
+                var result = openidm.action('external/rest', 'call', request);
+                logger.debug('OnCreate API call response: {}', result);
+
+                var resultJson = JSON.parse(result);
+                object.frIndexedString2 = resultJson.hash;
+
+                // Update related params so we know what type of BCrypt hash it is
+                object.frIndexedString3 = 'migrated';
+                object.frIndexedString5 = 'webfiling';
+            } catch (e) {
+                logger.error('OnCreate LEGACY PASSWORD not generated - API call status code {}', e.javaException.getCode());
+                logger.error('OnCreate LEGACY_PASSWORD error detail: {}', e);
+            }
         }
-        logger.info('OnCreate LEGACY_PASSWORD error detail: {}', e);
+        logger.info('OnCreate LEGACY_PASSWORD created!');
     }
+} catch (e) {
+    logger.error('OnCreate error detail: {}', e);
 }
 
 function getRandomHex(size) {
