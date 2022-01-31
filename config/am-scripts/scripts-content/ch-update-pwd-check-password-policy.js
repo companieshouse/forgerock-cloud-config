5f4bbd6c6dceac5567bc1c181d3d92f4b2925472
Steve Davis <sdavis@companieshouse.gov.uk>
2022-01-31 17:38:24 +0000

    idam-1262/Password policy changes to fix partial use of user attribute

--- config/am-scripts/scripts-content/ch-update-pwd-check-password-policy.js
+++ config/am-scripts/scripts-content/ch-update-pwd-check-password-policy.js
@@ -32,9 +32,11 @@ function setPolicyErrorMessage (policyResponse) {
     }));
 }
 
-function policyCompliant (pwd) {
+function policyCompliant (userObject, pwd) {
   _log('Checking password [' + pwd + '] against policy');
+
   var accessToken = transientState.get(ACCESS_TOKEN_STATE_FIELD);
+
   if (accessToken == null) {
     _log('Access token not in transient state');
     sharedState.put('errorMessage', 'Access token not in transient state');
@@ -42,12 +44,14 @@ function policyCompliant (pwd) {
   }
 
   var request = new org.forgerock.http.protocol.Request();
+
   var requestBodyJson = {
-    'object': {},
+    'object': userObject,
     'properties': {
       'password': pwd
     }
   };
+
   request.setMethod('POST');
   request.setUri(idmEndpoint);
   request.getHeaders().add('Authorization', 'Bearer ' + accessToken);
@@ -57,6 +61,7 @@ function policyCompliant (pwd) {
   var response = httpClient.send(request).get();
 
   logResponse(response);
+
   if (response.getStatus().getCode() === 200) {
     _log('200 response from IDM');
     var policyResponse = JSON.parse(response.getEntity().getString());
@@ -86,7 +91,88 @@ function policyCompliant (pwd) {
 
   _log('Error');
   sharedState.put('errorMessage', '[CHANGE PWD - POLICY CHECK] Error');
+  
   return NodeOutcome.ERROR;
+}
+
+function getUserObject () {
+  var ret = {};
+
+  try {
+    var activeUserId = _getUserIdFromSharedState();
+    var activeUserName = _getUserNameFromSharedState();
+
+    var userData = getUserData(activeUserName, activeUserId);
+
+    if (userData && userData.success && userData.user) {
+      ret.sn = userData.user.sn;
+      ret.givenName = userData.user.givenName;
+      ret.mail = userData.user.mail;
+      ret.userName = userData.user.userName;
+    }
+  } catch (e) {
+    _log('Error getting user object info ' + e);
+  }
+
+  _log('Treacle : ' + JSON.stringify(ret));
+  return ret;
+}
+
+function getUserData (email, id) {
+  try {
+    var searchTerm = email ? ('/openidm/managed/alpha_user?_queryFilter=userName+eq+%22' + email + '%22') : '/openidm/managed/alpha_user?_queryFilter=_id+eq+%22' + id + '%22';
+    _log('User Search term: ' + searchTerm);
+
+    var idmUserEndpoint = _fromConfig('FIDC_ENDPOINT') + searchTerm;
+    var request = new org.forgerock.http.protocol.Request();
+    var accessToken = transientState.get(ACCESS_TOKEN_STATE_FIELD);
+
+    if (accessToken == null) {
+      _log('Access token not in shared state');
+      return {
+        success: false,
+        message: 'Access token not in shared state'
+      };
+    }
+
+    request.setMethod('GET');
+    request.setUri(idmUserEndpoint);
+
+    request.getHeaders().add('Authorization', 'Bearer ' + accessToken);
+    request.getHeaders().add('Content-Type', 'application/json');
+    request.getHeaders().add('Accept-API-Version', 'resource=1.0');
+
+    var response = httpClient.send(request).get();
+
+    if (response.getStatus().getCode() === 200) {
+      var searchResponse = JSON.parse(response.getEntity().getString());
+      if (searchResponse && searchResponse.result && searchResponse.result.length > 0) {
+        _log('User found: ' + searchResponse.result[0].toString());
+        return {
+          success: true,
+          user: searchResponse.result[0]
+        };
+      } else {
+        _log('User NOT found: ' + email);
+        return {
+          success: false,
+          message: 'User NOT found: ' + email
+        };
+      }
+    } else {
+      _log('Error while checking user existence: ' + response.getStatus().getCode());
+      return {
+        success: false,
+        message: 'Error while checking user existence: ' + response.getStatus().getCode()
+      };
+    }
+  } catch (e) {
+    _log('Error : ' + e);
+    return {
+      success: false,
+      message: 'Error: ' + e
+    };
+  }
 }
 
 // Main execution path
@@ -102,7 +188,9 @@ if (newPassword == null) {
   outcome = NodeOutcome.ERROR;
 } else {
   try {
-    outcome = policyCompliant(newPassword);
+    var userObject = getUserObject();
+
+    outcome = policyCompliant(userObject, newPassword);
   } catch (e) {
     _log('error! ' + e);
     sharedState.put('errorMessage', e.toString());
