9308e1ddedc4bcd5fd05c26984e29bfa508de75c
jmiah <jmiah@companieshouse.gov.uk>
2022-09-06 17:21:57 +0100

    logging enhancement for when company filers are not found

--- config/idm-endpoints/scripts-content/scrs.js
+++ config/idm-endpoints/scripts-content/scrs.js
@@ -509,14 +509,20 @@
 
       return result;
     } catch (e) {
-      const code = e.javaException.code
-      const detail = e.javaException.detail ? JSON.parse(e.javaException.detail) : {}
 
-      if(code === 400 && (detail.body.error_code === "1000" || detail.body.error_code === "1001")){
-         _log('Did not find any filers for company : ' + companyNumber);
+      if(e.javaException){
+          const code = e.javaException.code;
+          const detail = e.javaException.detail ? JSON.parse(e.javaException.detail) : {};
+
+          if(code === 400 && (detail.body.error_code === "1000" || detail.body.error_code === "1001")){
+               _log('Did not find any filers for company : ' + companyNumber);
+          } else {
+               _log('Error in getCompanyEmails() call : ' + e);
+          }
       } else {
-         _log('Error in getCompanyEmails() call : ' + e);
+          _log('Error in getCompanyEmails() call : ' + e);
       }
+
       return {};
     }
   }
