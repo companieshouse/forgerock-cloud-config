b8ac9737d58aa44384c823291c0dd4c46cbcbef5
jmiahcompanieshouse <112622944+jmiahcompanieshouse@users.noreply.github.com>
2022-09-06 17:49:21 +0100

    logging enhancement for when company filers are not found (#429)
    * logging enhancement for when company filers are not found
    
    * logging enhancement for when company filers are not found
    
    * logging enhancement for when company filers are not found
    
    * logging enhancement for when company filers are not found

--- config/idm-endpoints/scripts-content/scrs.js
+++ config/idm-endpoints/scripts-content/scrs.js
@@ -509,7 +509,20 @@
 
       return result;
     } catch (e) {
-      _log('Error in getCompanyEmails() call : ' + e);
+
+      if(e.javaException){
+          const code = e.javaException.code;
+          const detail = e.javaException.detail ? JSON.parse(e.javaException.detail) : {};
+
+          if(code === 400 && (detail.body.error_code === "1000" || detail.body.error_code === "1001")){
+               _log('Error in calling filers endpoint for company ' + companyNumber + ' - code: ' + detail.body.error_code + ' - ' + detail.body.message);
+          } else {
+               _log('Error in getCompanyEmails() call : ' + e);
+          }
+      } else {
+          _log('Error in getCompanyEmails() call : ' + e);
+      }
+
       return {};
     }
   }
