bc8fc811f060096c851c74b31d11e0392d77bed0
paulholiday <pholiday@companieshouse.gov.uk>
2022-11-28 10:49:55 +0000

    Changing error logging to debug where personal data is logged out

--- config/connectors/mappings/event-scripts/webfilingAuthCode_alphaOrg_onError.js
+++ config/connectors/mappings/event-scripts/webfilingAuthCode_alphaOrg_onError.js
@@ -1,8 +1,8 @@
 // webfilingAuthCode_alphaOrg On Error
 logger.error('[CH-SYNC][AUTHCODE-TO-ALPHA-ORG][ERROR] Sync of webfilingAuthCode_alphaOrg with sourceId: ' + sourceId + ' failed with error: ' + error)
-logger.error('[CH-SYNC][AUTHCODE-TO-ALPHA-ORG][ERROR] Source object: ' + JSON.stringify(source))
+logger.debug('[CH-SYNC][AUTHCODE-TO-ALPHA-ORG][ERROR] Source object: ' + JSON.stringify(source))
 try {
-    logger.error('[CH-SYNC][AUTHCODE-TO-ALPHA-ORG][ERROR] Sync webfilingAuthCode_alphaOrg - error: ' + JSON.stringify(target))
+    logger.debug('[CH-SYNC][AUTHCODE-TO-ALPHA-ORG][ERROR] Sync webfilingAuthCode_alphaOrg - error: ' + JSON.stringify(target))
 } catch (e) {
     logger.error('[CH-SYNC][AUTHCODE-TO-ALPHA-ORG][ERROR] Error' + e);
 }
