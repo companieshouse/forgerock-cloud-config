3b6ca78494f35603513422e09c1a1765cf06e24a
jmiah <jmiah@companieshouse.gov.uk>
2023-06-12 12:47:25 +0100

    removed unused attribute

--- config/idm-endpoints/scripts-content/company.js
+++ config/idm-endpoints/scripts-content/company.js
@@ -328,7 +328,6 @@
         name: fullUser.givenName,
         email: fullUser.userName,
         displayName: fullUser.givenName ? fullUser.givenName : fullUser.userName,
-        phone: fullUser.telephoneNumber ? fullUser.telephoneNumber : undefined,
         membershipStatus: status ? status._refProperties.membershipStatus : StatusFilter.NONE
       });
     });
@@ -344,8 +343,6 @@
           _id: user._id,
           name: user.givenName,
           email: user.userName,
-          displayName: user.givenName ? user.givenName : user.userName,
-          phone: user.telephoneNumber ? user.telephoneNumber : undefined,
           displayName: user.givenName ? user.givenName : user.userName
         };
       }
